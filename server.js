b8975e533a9720b0d8cdbdcd72f68de15dc8deee
chardma3 <64539410+chardma3@users.noreply.github.com>
2023-06-11 20:48:25 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,177 @@
+const PORT = 8000;
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+const newspapers = [
+  {
+    name: 'bloomberg',
+    address: 'https://www.bloomberg.com',
+    base: ' https://www.bloomberg.com',
+  },
+  {
+    name: 'economist',
+    address: 'https://www.economist.com',
+    base: 'https://www.economist.com',
+  },
+  {
+    name: 'barrons',
+    address: ' https://www.barrons.com',
+    base: ' https://www.barrons.com',
+  },
+  {
+    name: 'forbes',
+    address: 'https://www.forbes.com',
+    base: 'https://www.forbes.com',
+  },
+  {
+    name: 'wallstreetjournal',
+    address: 'https://www.wsj.com',
+    base: 'https://www.wsj.com',
+  },
+  {
+    name: 'financialtimes',
+    address: 'https://www.ft.com',
+    base: 'https://www.ft.com',
+  },
+  {
+    name: 'guardian',
+    address: 'https://www.theguardian.com',
+    base: 'https://www.theguardian.com',
+  },
+  {
+    name: 'thetimes',
+    address: 'https://www.thetimes.co.uk',
+    base: 'https://www.thetimes.co.uk',
+  },
+  {
+    name: 'nyt',
+    address: 'https://www.nytimes.com/international',
+    base: 'https://www.nytimes.com',
+  },
+  {
+    name: 'latimes',
+    address: 'https://www.latimes.com/environment',
+    base: 'https://www.latimes.com',
+  },
+  {
+    name: 'smh',
+    address: 'https://www.smh.com.au',
+    base: 'https://www.smh.com.au',
+  },
+  {
+    name: 'bbc',
+    address: 'https://www.bbc.co.uk',
+    base: 'https://www.bbc.co.uk',
+  },
+  {
+    name: 'nyp',
+    address: 'https://nypost.com',
+    base: 'https://nypost.com',
+  },
+];
+
+app.get('/news', (req, res) => {
+  let articles = [];
+
+  Promise.all(
+    newspapers.map((newspaper) =>
+      axios
+        .get(newspaper.address)
+        .then((response) => {
+          const html = response.data;
+          const $ = cheerio.load(html);
+          $(
+            'a:contains("green") a:contains("startup"), a:contains("tech"), a:contains("climate"), a:contains("environment"),a:contains("AI"), a:contains("space"), a:contains("data"), a:contains("energy"), a:contains("stocks"), a:contains("investors")',
+            html
+          ).each(function () {
+            const title = $(this).text().trim();
+            let url = $(this).attr('href');
+            if (!url.includes('https')) {
+              url = `${newspaper.base}${url}`;
+            }
+            articles.push({
+              title,
+              url,
+              source: newspaper.name,
+            });
+          });
+          articles = articles.filter((article) => {
+            const { title } = article;
+            const words = title.split(' ');
+            return (
+              !title.includes('<') && words.length > 3 && words.length <= 20
+            );
+          });
+          articles = articles.filter(
+            (value, index, self) =>
+              index === self.findIndex((t) => t.url === value.url)
+          );
+        })
+        .catch((error) => {
+          console.error(`Error scraping ${newspaper.name}:`, error.message);
+        })
+    )
+  )
+    .then(() => {
+      res.json(articles);
+    })
+    .catch((error) => {
+      console.error('Error:', error.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
+});
+
+app.get('/news/:newspaperId', (req, res) => {
+  const newspaperId = req.params.newspaperId;
+  const selectedNewspaper = newspapers.find(
+    (newspaper) => newspaper.name === newspaperId
+  );
+
+  if (!selectedNewspaper) {
+    return res.status(404).json({ error: 'Newspaper not found' });
+  }
+
+  axios
+    .get(selectedNewspaper.address)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      let specificArticles = [];
+
+      $(
+        'a:contains("green") a:contains("startup"), a:contains("tech"), a:contains("climate"), a:contains("environment"),a:contains("AI"), a:contains("space"), a:contains("data"), a:contains("energy"), a:contains("stocks"), a:contains("investors")',
+        html
+      ).each(function () {
+        const title = $(this).text().trim();
+        let url = $(this).attr('href');
+        if (!url.includes('https')) {
+          url = `${selectedNewspaper.base}${url}`;
+        }
+        specificArticles.push({
+          title,
+          url,
+          source: newspaperId,
+        });
+      });
+      articles = articles.filter((article) => {
+        const { title } = article;
+        const words = title.split(' ');
+        return !title.includes('<') && words.length > 3 && words.length <= 20;
+      });
+      specificArticles = specificArticles.filter(
+        (value, index, self) =>
+          index === self.findIndex((t) => t.url === value.url)
+      );
+      res.json(specificArticles);
+    })
+    .catch((error) => {
+      console.error(`Error scraping ${selectedNewspaper.name}:`, error.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
+});
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
